d5c8f89da49fa28b9085791a8a2d6f5f385ace93
cultbaus <cultbaus@gmail.com>
2022-10-31 00:02:40 -0700

    feat(awesome): init

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,29 +1,24 @@
--- Unmuting audio before the volume signal is emitted will fix errors on startup.
--- require("awful").spawn.with_shell("pactl set-sink-mute @DEFAULT_SINK@ 0")
+local naughty = require 'naughty'
 
--- Define defaults
-c = {}
+if awesome.startup_errors then
+    naughty.notify {
+        preset = naughty.config.presets.critical,
+        title = 'error during startup',
+        text = awesome.startup_errors,
+    }
+end
 
-c.terminal = "alacritty"
-c.browser = "firefox"
-c.files = "nautilus"
-c.editor = os.getenv("EDITOR") or "nvim"
-c.editor_cmd = c.terminal .. " -e " .. c.editor
-c.modkey = "Mod4"
-
--- global config options
 C = {
-    terminal = "alacritty",
-    browser = "firefox",
-    editor = os.getenv("EDITOR") or "nvim",
-    app_launcher_cmd = "dmenu_run",
-    modkey = "Mod4",
-    altkey = "Mod1",
+    terminal = 'alacritty',
+    browser = 'firefox',
+    editor = os.getenv 'EDITOR' or 'nvim',
+    app_launcher_cmd = 'dmenu_run',
+    modkey = 'Mod4',
+    altkey = 'Mod1',
     neorg = true,
 }
 
--- Config
-require("awful.autofocus")
-require("themes.linear")
-require("config")
-require("signals")
+require 'awful.autofocus'
+require 'binds'
+require 'signals'
+require 'rules'
