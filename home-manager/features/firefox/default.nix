c2442bcb814506fe40c1c03d447231c9be739b85
cultbaus <cultbaus@gmail.com>
2022-11-03 07:56:42 -0700

    style(firefox): formatting

--- home-manager/features/firefox/default.nix
+++ home-manager/features/firefox/default.nix
@@ -16,24 +16,26 @@ in
           force = true;
           default = "DuckDuckGo";
           engines = {
-            "Styled DDG" = {
-              urls = [{ template = "https://duckduckgo.com/?q={searchTerms}&kae=t&kj=1d2021&kp=-2&k8=d4be98&kbc=1&kx=d8a657&kak=-1&ks=n&kn=-1&kau=-1&kk=-1&k1=-1&kao=-1&kz=-1&k21=1d2021&kap=-1&kf=-1&k9=a9b665&kax=-1&k7=1d2021&kaa=d3869b&kaq=-1&t=ha&va=j&ia=web"; }];
-              definedAliases = [ "@ddg" ];
-            };
             "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "type"; value = "packages"; }
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
               definedAliases = [ "@np" ];
             };
             "NixOS Wiki" = {
-              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+              urls = [
+                {
+                  template = "https://nixos.wiki/index.php?search={searchTerms}";
+                }
+              ];
               iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
+              updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "@nw" ];
             };
             "Bing".metaData.hidden = true;
