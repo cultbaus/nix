250a828d9134b0c3d9b3fe951ce36b2f8968665f
cultbaus <cultbaus@gmail.com>
2022-11-03 09:53:56 -0700

    feat(discord): add openasar overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,8 @@
 let
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  modifications = final: prev: { };
+  modifications = final: prev: {
+    discord = prev.discord.override { withOpenASAR = true; };
+  };
 in
 inputs.nixpkgs.lib.composeManyExtensions [ additions modifications ]
