2ad1d4a5d53e5f3a4a2434d9e5f647c1e0a76153
cultbaus <cultbaus@gmail.com>
2022-11-03 12:24:39 -0700

    feat(overlays): vim patching

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,6 +4,32 @@ let
 
   modifications = final: prev: {
     discord = prev.discord.override { withOpenASAR = true; };
+    vimPlugins = prev.vimPlugins {
+      nvim-treesitter = prev.vimPlugins.nvim-treesitter.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          ./nvim-treesitter-nix-injection.patch
+        ];
+        treesitterGrammars = prev.tree-sitter.withPlugins (p: with prev.vimPlugins; [
+          bash
+          css
+          docker
+          go
+          gomod
+          graphql
+          html
+          javascript
+          json
+          lua
+          make
+          norg
+          nix
+          prisma
+          rust
+          typescript
+          tsx
+        ]);
+      });
+    };
   };
 in
 inputs.nixpkgs.lib.composeManyExtensions [ additions modifications ]
